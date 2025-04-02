e419c408ce2b3950a5562d65d5853ada2afbfb94
Flape1 <117572436+Flape1@users.noreply.github.com>
2025-04-02 14:32:47 +0500

    Imported Keys

--- Untitled8.py
+++ Untitled8.py
@@ -6,6 +6,7 @@
 import streamlit as st
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
