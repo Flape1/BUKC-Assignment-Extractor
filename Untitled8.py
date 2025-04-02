e47d483763c16450806c5ec8f7845b652fa7c8ec
Flape1 <117572436+Flape1@users.noreply.github.com>
2025-04-02 14:33:28 +0500

    Update Untitled8.py

--- Untitled8.py
+++ Untitled8.py
@@ -11,6 +11,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import StaleElementReferenceException
 import time
 from bs4 import BeautifulSoup
 import pandas as pd
